541e6a6747a7d4f688b625071fed90ed56bb1bb2
Vijay-Birkur <vijay.b@comakeit.com>
2022-02-23 12:43:48 +0530

    Home page and Form page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import HcdHome from './components/HcdHome';
+import HcdTemplate from './components/HcdTemplate';
+import HcdForm from './components/HcdForm';
 
 function App() {
   return (
     <div className="App">
-      <HcdHome />
+      {/* <HcdTemplate /> */}
+      {/* <HcdHome /> */}
+      <HcdForm />
     </div>
   );
 }
