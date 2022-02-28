6a66c4d2a46ea148dc3a6d06e73a17744d79b923
Aditya-Kamarsu <aditya.k@comakeit.com>
2022-02-28 11:33:14 +0530

    minor changes

--- src/components/HcdHome.js
+++ src/components/HcdHome.js
@@ -82,7 +82,7 @@ const HcdHome = (props) => {
             <div className="input-group mb-3">
               <div className="input-group-prepend">
                 <span className="input-group-text" id="inputGroup-sizing-default">
-                  Client Name
+                  <b>Client Name</b>
                 </span>
               </div>
               <input
@@ -96,7 +96,7 @@ const HcdHome = (props) => {
             <div className="input-group mb-3">
               <div className="input-group-prepend">
                 <span className="input-group-text" id="inputGroup-sizing-default">
-                  Hiring Manager for coMakeIT
+                  <b>Hiring Manager for coMakeIT</b>
                 </span>
               </div>
               <input
@@ -120,7 +120,7 @@ const HcdHome = (props) => {
            </div>
 
             {
-              condition && <table class="table table-striped" style={{ width: '90%', border:'none' }}>
+              condition && <table className="table table-striped" >
                 <thead>
                   <tr>
                     <th><b>CANDIDATE NAME</b></th>
@@ -133,7 +133,7 @@ const HcdHome = (props) => {
 
                   {
                     empdata.map((element) => {
-                      return <tr class="table" key={element.name}>
+                      return <tr className="table" key={element.name}>
                         <td>{element.candidatename}</td>
                         <td>{element.applicablevacancy}</td>
                         <td>{element.hourlyrate}</td>
@@ -148,21 +148,10 @@ const HcdHome = (props) => {
 
             }
             <br />
-            {/* <button
-              onClick={() => setShowEmp(!showEmp)}
-              type="button"
-              className="btn btn-sm btn-primary ml-auto"
-              data-bs-toggle="modal"
-              data-bs-target="#exampleModal"
-            >
-              {!showEmp}
-              {showEmp}
-              <i className="fas fa-plus me-2"></i>Add Employee
-            </button> */}
             <button
             type="button"
             style={{"color":"white","fontSize":"20px","marginLeft":  "10px","width":"98%"}}
-            class="btn btn-dark"
+            className="btn btn-dark"
         onClick={() => {
           setModalOpen(true);
         }}
