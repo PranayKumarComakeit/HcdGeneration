aff0fb842489c77087b6a4823a8ef7c50adba226
Vijay-Birkur <vijay.b@comakeit.com>
2022-03-22 15:59:19 +0530

    app change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import HcdHome from './components/HcdHome';
 import HcdTemplate from './components/HcdTemplate';
@@ -9,7 +9,6 @@ import {
 } from "react-router-dom";
 import { Routes, Route } from 'react-router-dom';
 import HcdHourlyHome from './components/HcdHourlyHome';
-import HcdHourlyForm from './components/HcdHourlyForm';
 import HCDHourlyTemplate from './components/HCDHourlyTemplate';
 import ErrorPage from './components/ErrorPage';
 function App() {
@@ -23,17 +22,13 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <Routes>
-      <Route exact path="/" element={<HcdHome datatoApp={datatoApp} />}/>
+         <Route exact path="/" element={<HcdHome datatoApp={datatoApp} />}/>
          <Route exact path="/HCDHourly" element={<HcdHourlyHome datatoApp={datatoApp} />}/>
          <Route exact path="/OpenTemplate" element={<HcdTemplate data={data} />}/>
          <Route exact path="/HourlyTemplate" element={<HCDHourlyTemplate data={data} />}/>
          <Route exact path="/Error" element={<ErrorPage />}/>
       </Routes>
       </BrowserRouter>
-
-{/* {!condition && <HcdHome datatoApp={datatoApp} />}
-      {condition && <HcdTemplate data={data} condition={"Pranay"}/>}
-       */}
     </div>
   );
 }
